82bcd9a6077d3286f5766b2b718fa9de54bf3a0d
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-09 15:31:39 -0300

    updateproject

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-memory-game/',
+  base: '/jogodamemoria/',
   plugins: [vue()],
   resolve: {
     alias: {
